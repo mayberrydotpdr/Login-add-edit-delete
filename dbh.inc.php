c9eb1abfae8799113e6b1d1e1ca2e458ec1383be
Jason Mayberry <mayberry.pdr@gmail.com>
2017-12-28 15:57:25 -0600

    Create dbh.inc.php

---
+++ dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "vaportrail.asoshared.com";
+$dbUsername = "workmans_Jason";
+$dbPassword = "Smith&Wesson";
+$dbName = "workmans_db";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
