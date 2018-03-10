5e939dc193b9d676b0efcd229049087acd92384c
Jason Mayberry <mayberry.pdr@gmail.com>
2018-03-10 11:48:11 -0600

    Create connect-db.php

---
+++ connect-db.php
@@ -0,0 +1,8 @@
+<?php
+$server = "vaportrail.asoshared.com";
+$user = "workmans_Jason";
+$pass = "Password";
+$db = "workmans_offices";
+$mysqli = new mysqli($server, $user, $pass, $db);
+// show errors (remove this line if on a live site)
+mysqli_report(MYSQLI_REPORT_ERROR);
