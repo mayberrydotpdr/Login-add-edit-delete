8e1c6b9681c078d06080baa7a7a7a673fe1bf2f2
Jason Mayberry <mayberry.pdr@gmail.com>
2018-03-10 11:49:37 -0600

    Delete dbh.inc.php

--- dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "vaportrail.asoshared.com";
-$dbUsername = "workmans_Jason";
-$dbPassword = "Password";
-$dbName = "workmans_db";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
